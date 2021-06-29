fb440ea3a0fcd5666675c8e2fa28dcea741d6019
Alice Nguyen <alice.n.69521@gmail.com>
2021-06-28 17:25:01 -0700

    Week 2 Workshop Assignment

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -14,7 +14,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-    postFavorite: campsiteId => (postFavorite(campsiteId))
+    postFavorite: campsiteId => (postFavorite(campsiteId)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
 };
 
 function RenderComments({comments}) {
@@ -23,7 +24,13 @@ function RenderComments({comments}) {
         return (
             <View style={{margin: 10}}>
                 <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Rating 
+                    type='star'
+                    style={{alignItems: 'flex-start', paddingVertical:'5%'}}
+                    startValue={item.rating}
+                    imageSize={10}
+                    readonly
+                />
                 <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         );
@@ -52,15 +59,25 @@ function RenderCampsite(props) {
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
-                <Icon
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() => props.favorite ? 
-                        console.log('Already set as a favorite') : props.markFavorite()}
-                />
+                <View style={styles.cardRow}>
+                    <Icon
+                        name={props.favorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() => props.favorite ? 
+                            console.log('Already set as a favorite') : props.markFavorite()}
+                    />
+                    <Icon
+                        name={'pencil'}
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={() => props.onShowModal()}
+                    />
+                </View>
             </Card>
         );
     }
@@ -68,6 +85,35 @@ function RenderCampsite(props) {
 }
 
 class CampsiteInfo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showModal: false,
+            rating: 5,
+            author: '',
+            text: ''
+        };
+    }
+
+    toggleModal() {
+        this.setState({showModal: !this.state.showModal});
+    }
+
+    handleComment(campsiteId) {
+        this.props.postComment(campsiteId, this.state.rating, this.state.author, this.state.text);
+        this.toggleModal();
+    }
+
+    resetForm() {
+        this.setState({
+            rating: 5,
+            author: '',
+            text: '',
+            showModal: false
+        });
+    }
 
     static navigationOptions = {
         title: 'Campsite Information'
@@ -86,11 +132,91 @@ class CampsiteInfo extends Component {
                 <RenderCampsite campsite={campsite}
                     favorite={this.props.favorites.includes(campsiteId)}
                     markFavorite={() => this.markFavorite(campsiteId)}
+                    onShowModal={() => this.toggleModal()}
                 />
                 <RenderComments comments={comments} />
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}
+                >
+                    <View>
+                    <Rating 
+                        showRating
+                        startingValue={this.state.rating}
+                        imageSize={40}
+                        style={{paddingVertical: 10}}
+                        onFinishRating={rating => this.setState({rating: rating})} 
+                        type={'star'}
+                    />
+                    <Input
+                        placeholder='Your Name Here'
+                        leftIcon={
+                            <Icon
+                            name='user-o'
+                            type='font-awesome'
+                            size={24}
+                            color='black'
+                            />
+                        }
+                        leftIconContainerStyle={{paddingRight:10}}
+                        onChangeText={author => this.setState({author: author})}
+                        value={this.state.author}
+                    />
+                    <Input
+                        placeholder='Your Comment Here'
+                        leftIcon={
+                            <Icon
+                            name='comment-o'
+                            type='font-awesome'
+                            size={24}
+                            color='black'
+                            />
+                        }
+                        leftIconContainerStyle={{paddingRight:10}}
+                        onChangeText={text => this.setState({text: text})}
+                        value={this.state.text}
+                    />
+                    </View>
+                    <View style={styles.modal} style={{margin: 10}}>
+                        <Button
+                            title='Submit'
+                            color='#5637DD'
+                            onPress={() => {
+                                this.handleComment(campsiteId);
+                                this.resetForm();
+                            }}
+                        />
+                    </View>
+                    <View style={styles.modal} style={{margin: 10}}>
+                        <Button
+                            onPress={() => {
+                                this.toggleModal();
+                                this.resetForm();
+                            }}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
+                </Modal>
             </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    modal: { 
+        justifyContent: 'center',
+        margin: 20
+    },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
--- redux/comments.js
+++ redux/comments.js
@@ -1,3 +1,4 @@
+import { startClock } from 'react-native-reanimated';
 import * as ActionTypes from './ActionTypes';
 
 export const comments = (state = { errMess: null, comments: []}, action) => {
@@ -7,6 +8,11 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
 
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
+
+        case ActionTypes.ADD_COMMENT:
+            const comment = action.payload;
+            comment.id = state.comments.length;
+            return {...state, comments: state.comments.concat(comment)};
 
         default:
             return state;
