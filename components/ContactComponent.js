872cb751e1da091fcb35e201bda9cb5690860cc7
Alice Nguyen <alice.n.69521@gmail.com>
2021-07-06 21:51:19 -0700

    Social Sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
     
@@ -9,6 +10,14 @@ class Contact extends Component {
         title: 'Contact Us'
     }
     
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     render () {
         return (
             <ScrollView>
@@ -19,7 +28,18 @@ class Contact extends Component {
                         <Text style={{marginBottom: 10}}>U.S.A.</Text>
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email: campsites@nucamp.co</Text>
-                        </Card>
+                    </Card>
+                    <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                 </Animatable.View>
             </ScrollView>
         )
